44574bc652d094bf71b6d525f336fa4378b73e2b
Akin909 <akin.sowemimo@gmail.com>
2018-03-07 05:23:41 +0000

    add root files config option

--- browser/src/Services/Configuration/DefaultConfiguration.ts
+++ browser/src/Services/Configuration/DefaultConfiguration.ts
@@ -187,6 +187,7 @@ const BaseConfiguration: IConfigurationValues = {
     "language.ocaml.languageServer.arguments": ["--stdio"],
     "language.ocaml.languageServer.configuration": ocamlAndReasonConfiguration,
 
+    "language.typescript.rootFiles": ["tsconfig.json", "package.json"],
     "language.typescript.languageServer.command": "typescript-language-server",
     "language.typescript.languageServer.arguments": ["--stdio"],
     "language.typescript.completionTriggerCharacters": [".", "/", "\\"],
