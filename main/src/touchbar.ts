df37444e7eca2ea8e6b5716e2a7e198be6206892
Akin909 <akin.sowemimo@gmail.com>
2018-04-24 07:10:47 +0100

    add scrubber component

--- main/src/touchbar.ts
+++ main/src/touchbar.ts
@@ -1,9 +1,9 @@
-import { BrowserWindow, TouchBar } from "electron"
+import { BrowserWindow, TouchBar, TouchBarScrubber } from "electron"
 import * as flatten from "lodash/flatten"
 
 const { TouchBarButton /* , TouchBarLabel */, TouchBarSpacer } = TouchBar
 
-const dummyBuffers = ["dummy1", "dummy1", "dummy1", "dummy1"]
+const dummyBuffers = ["file1.txt", "file2.txt", "file3.txt", "file4.txt"]
 
 const buttons = (buffers: string[]) =>
     buffers.map(
@@ -18,9 +18,26 @@ const buttons = (buffers: string[]) =>
     )
 
 const createTouchBarMenu = (browserWindow: BrowserWindow, buffers = dummyBuffers) => {
+    // List needs to be scrollable
+
+    // const scrubber = new TouchBarScrubber({
+    //     items: dummyBuffers.map(buffer => ({ label: buffer })),
+    //     highlight: null,
+    //     selectedStyle: null,
+    //     overlayStyle: null,
+    //     showArrowButtons: false,
+    //     mode: "free",
+    //     continuous: true,
+    //     select: selectedIndex => {
+    //         console.log(selectedIndex)
+    //     },
+    // })
+
     const arrangement = flatten(
         buttons(buffers).map(button => [button, new TouchBarSpacer({ size: "small" })]),
     )
+
+    // const touchBar = new TouchBar({ items: [scrubber] })
     const touchBar = new TouchBar({ items: arrangement })
 
     browserWindow.setTouchBar(touchBar)
