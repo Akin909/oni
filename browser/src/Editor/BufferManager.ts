ede269384bb2b3a938de87e9478ea2fd117a5f5d
Akin909 <akin.sowemimo@gmail.com>
2018-05-20 21:02:05 +0100

    remove console.log

--- browser/src/Editor/BufferManager.ts
+++ browser/src/Editor/BufferManager.ts
@@ -254,8 +254,6 @@ export class Buffer implements IBuffer {
 
     public async applyTextEdits(textEdits: types.TextEdit | types.TextEdit[]): Promise<void> {
         const textEditsAsArray = textEdits instanceof Array ? textEdits : [textEdits]
-        console.log("textEditsAsArray: ", textEditsAsArray)
-
         const sortedEdits = LanguageManager.sortTextEdits(textEditsAsArray)
 
         const deferredEdits = sortedEdits.map(te => {
